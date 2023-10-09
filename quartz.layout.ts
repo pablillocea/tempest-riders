fa52e40ca2678d779dcd8f93ddd22b017f214d01
Pablo Lillo Cea <pablillocea@gmail.com>
2023-10-09 18:50:39 +0200

    Quartz sync: Oct 9, 2023, 6:50 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+     "DnD Beyond": "https://www.dndbeyond.com/campaigns/4062536", 
     },
   }),
 }
