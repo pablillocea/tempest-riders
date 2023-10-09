52f959f242492d75621be71cd591b97fd787ba54
Pablo Lillo Cea <pablillocea@gmail.com>
2023-10-09 19:02:48 +0200

    New colours.

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,6 +18,28 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
+          light: '#F9F2E3',  // Aged parchment
+    	  lightgray: '#D4C3A5',  // Weathered wood
+    	  gray: '#967259',  // Aged leather
+    	  darkgray: '#4F3822',  // Darkened wood
+    	  dark: '#1C140D',  // Near black
+     	  secondary: '#AA6B39',  // Rustic gold
+     	  tertiary: '#3C2F2F',  // Aged iron
+      	  highlight: 'rgba(170, 107, 57, 0.15)',  // Translucent rustic gold
+  	},
+  	darkMode: {
+    	  light: '#2C2C2C',  // Smoky black
+    	  lightgray: '#4E4E4E',  // Charcoal
+    	  gray: '#757575',  // Mid grey
+    	  darkgrkay: '#A9A9A9',  // Light grey
+    	  dark: '#FDFDFD',  // Near white
+    	  secondary: '#AA6B39',  // Rustic gold
+    	  tertiary: '#3C2F2F',  // Aged iron
+    	  highlight: 'rgba(170, 107, 57, 0.15)',  // Translucent rustic gold
+  	},
+      /**
+      colors: {
+        lightMode: {
           light: "#faf8f8",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
@@ -36,7 +58,7 @@ const config: QuartzConfig = {
           secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
-        },
+        },*/
       },
     },
   },
